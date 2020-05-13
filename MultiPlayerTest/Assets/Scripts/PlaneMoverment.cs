62d8c6abee4a6427a207a0340dbd53ad0e6e88dd
tocngodaden1 <K155520214012@tnut.edu.vn>
2020-05-13 14:20:48 +0700

    add project

---
+++ MultiPlayerTest/Assets/Scripts/PlaneMoverment.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlaneMoverment : NetworkBehaviour
+{
+    [SerializeField] private float speed;
+
+    private void FixedUpdate()
+    {
+        if (this.isLocalPlayer)
+        {
+            float movermentX = Input.GetAxis("Horizontal");
+            //float movermentY = Input.GetAxis("Vertical");
+            GetComponent<Rigidbody2D>().velocity = new Vector2(movermentX * speed, 0.0f);
+            //GetComponent<Rigidbody2D>().velocity = new Vector2(0.0f ,movermentY * speed);
+        }
+    }
+}
